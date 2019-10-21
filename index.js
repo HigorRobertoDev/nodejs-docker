f6263f595602a19e6483e80581de5e1864081c4d
Higor Roberto <h.saposilva@gmail.com>
2019-10-21 15:31:42 -0300

    Correção index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const HOST = '0.0.0.0';
 
 const app = express();
 
-app.get('/', (req, res)) => {
-    res.send("Hello world");
-}
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
 
 app.listen(PORT, HOST);
